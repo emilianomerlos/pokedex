38c050a695b2e64d899ee4883768ce92b0459a73
Emiliano Notebook <merlosemiliano@gmail.com>
2020-06-17 15:26:39 -0300

    last metas and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -9,13 +9,17 @@ export default function MyApp({ Component, pageProps }) {
 			<Head>
 				<title>Pokedex</title>
 				<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet"></link>
+				
+				<link rel="icon" type="image/svg+xml" href="/favicon.svg">
+				<link rel="alternate icon" href="/favicon.ico"></link>
+
 				<meta property="og:title" content="Pokedex" />
 				<meta property="og:description" content="Gotta Catch 'Em All" />
 				<meta property="og:image" content="/imgs/pokedex-compressor.png" />
-				<meta property='twitter:title' content="Pokedex"/>
-				<meta property='twitter:image' content="/imgs/pokedex-compressor.png" />
-				<meta property="twitter:description" content="Gotta Catch 'Em All" />
 				<meta name="twitter:card" content="summary_large_image"/>
+				<meta name='twitter:title' content="Pokedex"/>
+				<meta name='twitter:image' content="/imgs/pokedex-compressor.png" />
+				<meta name="twitter:description" content="Gotta Catch 'Em All" />
 			</Head>
 			<Component {...pageProps} />
 		</Layout>
