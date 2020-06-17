6a398416f35590d681fa45081010701158c54e85
Emiliano Notebook <merlosemiliano@gmail.com>
2020-06-17 15:27:59 -0300

    last metas and favicon bis

--- pages/_app.js
+++ pages/_app.js
@@ -10,8 +10,8 @@ export default function MyApp({ Component, pageProps }) {
 				<title>Pokedex</title>
 				<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet"></link>
 				
-				<link rel="icon" type="image/svg+xml" href="/favicon.svg">
-				<link rel="alternate icon" href="/favicon.ico"></link>
+				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+				<link rel="alternate icon" href="/favicon.ico" />
 
 				<meta property="og:title" content="Pokedex" />
 				<meta property="og:description" content="Gotta Catch 'Em All" />
