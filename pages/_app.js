7470774a0fdef0e4747845a435dc31a2404c01f5
Emiliano Notebook <merlosemiliano@gmail.com>
2020-06-17 15:06:06 -0300

    more and more meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ export default function MyApp({ Component, pageProps }) {
 				<meta property="og:title" content="Pokedex" />
 				<meta property="og:description" content="Gotta Catch 'Em All" />
 				<meta property="og:image" content="/imgs/pokedex-compressor.png" />
+				<meta name="twitter:card" content="summary_large_image"/>
 			</Head>
 			<Component {...pageProps} />
 		</Layout>
